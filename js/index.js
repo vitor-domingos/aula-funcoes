4d944126925f8dedccd3c7f037cefb16a63aa053
Vitor Domingos <vm486092@gmail.com>
2023-10-19 16:59:22 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,57 @@
+
+
+
+
+// Função que exibe um alerta de boas-vindas
+function boasVindasDaPagina() {
+  window.alert("Seja muito bem-vindo à minha página na web, onde eu apresento as funções no JavaScript.");
+}
+
+// Chamando a função de boas-vindas
+boasVindasDaPagina();
+
+// Função que multiplica cinco números
+function multiplicacaoEntreCincoNumeros(firstnumber, secondnumber, thirdnumber, fournumber, fivenumber) {
+  console.log(firstnumber * secondnumber * thirdnumber * fournumber * fivenumber);
+}
+
+// Chamando a função de multiplicação com números específicos
+multiplicacaoEntreCincoNumeros(5, 10, 15, 20, 25);
+
+// Função que retorna uma concatenação de linguagens de programação
+function listaDeLinguagensDeProgramacaoFrontEnd(firstlanguage, secondlanguage, thirdlanguage, fourlanguage, fivelanguage) {
+  return firstlanguage + secondlanguage + thirdlanguage + fourlanguage + fivelanguage;
+}
+
+// Definindo variáveis para as linguagens de programação
+const HTML = "HTML";
+const CSS = "CSS";
+const JS = "JavaScript";
+const TS = "TypeScript";
+const PY = "Python";
+
+// Chamando a função e armazenando o resultado em uma variável
+const valorDeLinguagensDeProgramacaoFrontEnd = listaDeLinguagensDeProgramacaoFrontEnd(HTML, CSS, JS, TS, PY);
+
+// Exibindo o valor retornado no console
+console.log(valorDeLinguagensDeProgramacaoFrontEnd);
+
+// Função que retorna uma concatenação de profissões na área de tecnologia
+function profissoesNaAreaDaTecnologia(firstprofession, secondprofession, thirdprofession, fourprofession, fiveprofession) {
+  return firstprofession + secondprofession + thirdprofession + fourprofession + fiveprofession;
+}
+
+// Definindo variáveis para as profissões
+const desenvolvedorDeSoftware = "Desenvolvedor de Software";
+const administradorDeBancoDeDados = "Administrador de Banco de Dados";
+const analistasDeSistemasDeComputacao = "Analistas de Sistemas de Computação";
+const uiDesigner = "UI Designer";
+const devOps = "DevOps";
+
+// Chamando a função e armazenando o resultado em uma variável
+const valorDasProfissoesNaAreaDaTecnologia = profissoesNaAreaDaTecnologia(desenvolvedorDeSoftware, administradorDeBancoDeDados, analistasDeSistemasDeComputacao, uiDesigner, devOps);
+
+// Exibindo o valor retornado no console
+console.log(valorDasProfissoesNaAreaDaTecnologia);
+
+
